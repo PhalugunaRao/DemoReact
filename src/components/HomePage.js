51c58b91602c5dbe2d0ca13bf3df19d1654d2657
phalu rao <phalu900@gmail.com>
2019-07-08 22:29:25 +0530

    Simple navgation page to page

---
+++ src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function HomePage() {
+    return(
+    <div className="jumbotron">
+    <h1>Demo one</h1>
+    <p> Responsive web apps</p>
+    <a href="/about">About</a>
+
+    </div>
+    );
+    
+}
+export default HomePage;
